9b2826cadbcdf2e5e3e6aed85c218594cfe6ecfd
Yuichiro.Takiyama <yuichiro0411@me.com>
2018-11-20 21:22:36 +0900

    Edit Group division.
    I’m sorry for commit many files.

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,37 @@
+class GroupsController < ApplicationController
+	before_action :set_group, only: [:edit, :update]
+	def index
+	end
+	def new #新規グループを作成する。
+	  @group = Group.new #Groupモデルの新しいインスタンスを定義
+	  @group.users << current_user #現在のユーザーをグループに保存
+	end
+
+	def create
+		@group = Group.new(group_params)
+		if @group.save #グループ作成の可否で条件分岐
+			redirect_to root_path, notice: 'グループを作成しました。'
+		else
+			render :new
+		end
+	end
+
+		#redirect_toがHTTPリクエストを送り、そのレスポンスとして返ってくるビューを表示するのに対し、renderはHTTPリクエストを送らず、該当するビューだけを表示.
+	def update
+		if @group.update(group_params)
+		  redirect_to group_messages_path(@group), notice: 'グループを編集しました。'
+	    else
+	    	render :edit
+	    end
+	end
+
+private
+	def group_params
+		params.require(:group).permit(:name, { :user_ids => [] })
+	end
+
+	def set_group
+		@group = Group.find(params[:id])
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     devise_for :users
-    root 'messages#index'
-    resources :users, only: [:edit, :update]    #ユーザー編集機能
+    root 'groups#index'
+    resources :users, only: [:index, :edit, :update]
+    resources :groups, only: [:new, :create, :edit, :update] do
+      resources :messages, only: [:index, :create] #indexアクションで一覧を表示
+    end
 end
+#グループ機能では新規作成と編集機能がある。新規投稿画面を表示する：new,編集画面を表示する;edit
+#
