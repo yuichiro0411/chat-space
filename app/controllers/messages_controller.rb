0c508e03334c8180cbed1cfb0393309f29fa8ba8
Yuichiro.Takiyama <yuichiro0411@me.com>
2018-11-22 18:58:20 +0900

    Added image_form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,33 @@
 class MessagesController < ApplicationController
+before_action :set_group
 
 def index
+	@message =  Message.new #Messageモデルの新しいインスタンス作成
+	@messages = @group.messages.includes(:user)
+	# グループに所属するすべてのメッセージを定義。n＋1問題を避けるために
 end
+
 def create
+	@message = @group.messages.new(message_params)
+	if  @message.save
+		redirect_to group_message_path(@group), notice:'メッセージをが送信されました。'
+		#保存に成功した場合、メッセージを出してチャットページに移動。
+	else
+		@messages = @group.messages.incldes(:user)
+		flash.now[:alert] = 'メッセージを入力してください'
+	    render :index
+#保存できなかった場合
+	end
 end
-def new
+
+private
+
+def message_params
+	params.require(:massage).permit(:content, :image).merge(user_id: current_user.id)
+end
+
+def set_group
+	@group = Group.find(params[:group_id])
 end
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,27 @@
 class Group < ApplicationRecord
 	has_many :group_users
 	has_many :users, through: :group_users
+	has_many :messages
 	validates :name, presence: true
+
+	def show_last_message
+	  if (last_message = message.last).present?
+	  	last_message.content_? ? last_message.content : '画像が投稿されています'
+	  else
+	  	'まだメッセージはありません'
+	  end
+	end
 end
+
+#イドバーのグループ部分に、最新のメッセージを表示できるようにしましょう。
+
+#最新のメッセージについて、文章が投稿されている場合、
+#画像が投稿されている場合、まだ投稿がされていない場合が考えられます。
+#かといって、ビュー部分にif、while、caseなどを用いて条件分岐を書いてしまうと、ビューのコードが複雑になってしまい、読みづらくなってしまいます。
+#このような場合には、モデルにインスタンスメソッドを定義することで、ビューの記述をシンプルにすることができます。
+#今回はapp/models/group.rbにインスタンスメソッドを定義してみましょう。
+#if (last_message = messages.last).present?」と記述することで、最新のメッセージを変数last_messageに代入しつつ、メッセージが投稿されているかどうかで場合分けを行なっています。
+
+#メッセージが投稿されている場合の内部で、さらに文章が投稿されている場合、画像が投稿されている場合で処理を分けています。
+#９行目の記述は三項演算子と呼ばれるもので、下記のような文法を取ります。
+#条件式 ? trueの時の値 : falseの時の値
