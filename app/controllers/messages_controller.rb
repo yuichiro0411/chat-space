d13f4443548cd6f7a29a593a03fbc81bec829f7a
Yuichiro.Takiyama <yuichiro0411@me.com>
2018-11-23 21:26:43 +0900

    Adjusted spaces

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,33 +1,33 @@
 class MessagesController < ApplicationController
 before_action :set_group
 
-def index
-	@message =  Message.new #Messageモデルの新しいインスタンス作成
-	@messages = @group.messages.includes(:user)
+  def index
+    @message =  Message.new #Messageモデルの新しいインスタンス作成
+    @messages = @group.messages.includes(:user)
 	# グループに所属するすべてのメッセージを定義。n＋1問題を避けるために
-end
+  end
 
-def create
-	@message = @group.messages.new(message_params)
-	if  @message.save
-		redirect_to group_messages_path(@group), notice:'メッセージをが送信されました。'
-		#保存に成功した場合、メッセージを出してチャットページに移動。
-	else
-		@messages = @group.messages.incldes(:user)
-		flash.now[:alert] = 'メッセージを入力してください'
-	    render :index
+  def create
+    @message = @group.messages.new(message_params)
+    if  @message.save
+        redirect_to group_messages_path(@group), notice:'メッセージをが送信されました。'
+	    #保存に成功した場合、メッセージを出してチャットページに移動。
+    else
+      @messages = @group.messages.incldes(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
 #保存できなかった場合
-	end
-end
+    end
+  end
 
-private
+  private
 
-def message_params
-	params.require(:massage).permit(:content, :image).merge(user_id: current_user.id)
-end
+  def message_params
+    params.require(:massage).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
-def set_group
-	@group = Group.find(params[:group_id])
-end
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-	def edit
-	end
+  def edit
+  end
 
-	def update #保存できた場合、できない場合で条件分岐
-		if current_user.update(user_params)
-			redirect_to root_path
-		else
-			render :edit
-		end
-	end
+  def update #保存できた場合、できない場合で条件分岐
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
-	private
+private
 
-	def user_params
-		params.require(:user).permit(:name. :email)
-	end
+  def user_params
+    params.require(:user).permit(:name. :email)
+  end
 end
