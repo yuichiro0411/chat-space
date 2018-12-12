a401908e3ec63c7877cd4d5f70447c006f480783
Yuichiro.Takiyama <yuichiro0411@me.com>
2018-12-12 20:08:35 +0900

    Making asynchronous connection

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,27 +1,59 @@
-$(function(){
-	function buildHTML(data){
+$(document).on('turbolinks:load', function(){
+  function buildHTML(data){
 
-	}
-  var formData = new FormData(form);
+  	var html = `<div class= 'message'>
+  	              <div class= 'upper-message'>
+  	                <div class= 'upper-message__user-name'>
+  	                  ${data.name}
+  	                </div>
+  	                <div class='upper-message__date'>
+  	                  ${data.created_at}
+  	                </div>
+  	              </div>
+                  <div class= 'lower-message'>
+                    <p class= 'lower-message__content'>
+                      ${data.content}
+                    </p>
+                    <image= src='/message.image.url' class='lower-message__image' if message.image.present?>
+                  </div>
+                </div>`;
+  	return html;
+  }
 
-  $('footer__form-area').submit(function(e){
+  $('#form').on('submit', function(e){
   	e.preventDefault();
-    $.ajax()
+    var formData = new FormData(this);
+  	console.log(this)
+  	var url = $(this).attr('action')
+    // var message = $('#form').val('');
+    $.ajax({
       type: 'POST',
-      url: '/groups/:group_id/messages',
+      url: url,
       data: formData,
       processData: false,
-      contentType: false
+      contentType: false,
+      dataType:'json'
+    })
     .done(function(data){
-      var html = buildHTML(data)
-      $('.message').append(html)
+      // console.log(data)
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.footer__form-area-message').val('')
+
+      // console.log('送信完了！');
       // $('セレクタ').append('追加したいもの')で、操作された時に後から文章などを追加表示できる。
       // 今回はhtml、つまりbuildHTMLされたものが後から追加される。
-      // $('.right-content').animate({scrollTop})
-      $('.right-content').animate({scrollTop: $('.right-content')},'fast');
-      })
-     .fail(function(data){
-        alert("OMG!!error!! Couldn't post");
-      });
+      // $('messages').animate(scrollBottom(0));
+      function scrollTobottom(){
+        var height = $('.right-content__chat-mainspace')[0].scrollHeight;
+        var bottom = $('.right-content__chat-mainspace');
+        bottom.animate({scrollTop: height},'fast','swing');
+      };
+      scrollTobottom();
+    })
+    .fail(function(){
+      alert("OMG!!error!! Couldn't post");
+    });
+    return false;
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,7 +1,8 @@
-json.message @message do |message|
-  json.content message.content
-  json.image message.image
-  json.group_id message.group_id
-  json.user_id message.user_id
-  json.created_at message.crrated_at
-end
+json.content @message.content
+json.image @message.image
+json.group_id @message.group_id
+json.user_id @message.user_id
+json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+json.name @message.user.name
+
+
