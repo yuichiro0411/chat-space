47ecc41a44d503c7ed85fde61ab335cccfa1065d
Yuichiro.Takiyama <yuichiro0411@me.com>
2018-12-17 18:38:18 +0900

    Added edit incremental search.

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,28 +1,30 @@
-$(document).on('turbolinks:load',function(){
-console.log('発火確認！')
-  var form = $('#user-search-field');
+$(document).on('turbolinks:load', function(){
+  console.log('発火確認！')
   function appendUser(user){
   	var html = `<li>
   	              <div class="chat-group-user clearfix">
-                    <p class="chat-group-user__name">${user.name}</p>
-                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                    <p class="chat-group-user__name"> ${user.name} </p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id= ${user.id}, data-user-name= ${user.name} >追加</a>
                   </div>
                 </li>`
     ('.chat-group-user__name').append(html);
-  }
+  };
+
   function appendNoUser(user){
     var html =`>`
-  }
-
+  };
+  var form = $('#user-search-field');
   form.on('keyup', function(e){
-  	e.preventDefault
+  	e.preventDefault();
     var input = form.val();
 
     $.ajax({
       type: 'GET',
       url: '/users',
       data: { keyword: input },
-      dataType: 'json'
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
     .done(function(user){
       if (user.length !== 0){
